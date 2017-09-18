eb95025992913798b0cad0c3669be28808161412
郭春城 <gccios@sina.com>
2017-09-18 10:43:19 +0800

    增加工程文件

---
+++ ZhiXiang/ZhiXiang/Views/CollectionViewCell/Home/HomeCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.h
+//  ZhiXiang
+//
+//  Created by 郭春城 on 2017/9/18.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ ZhiXiang/ZhiXiang/Views/CollectionViewCell/Home/HomeCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.m
+//  ZhiXiang
+//
+//  Created by 郭春城 on 2017/9/18.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import "HomeCollectionViewCell.h"
+
+@implementation HomeCollectionViewCell
+
+@end
