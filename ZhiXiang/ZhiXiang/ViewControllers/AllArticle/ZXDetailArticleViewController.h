e7608d5da38a7816ee247498fc797b699c4a524b
beimingyouyu0823 <haipeng0823@126.com>
2017-09-20 17:28:42 +0800

    添加类

---
+++ ZhiXiang/ZhiXiang/ViewControllers/AllArticle/ZXDetailArticleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZXDetailArticleViewController.h
+//  ZhiXiang
+//
+//  Created by 王海朋 on 2017/9/20.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import "ZXBaseViewController.h"
+
+@interface ZXDetailArticleViewController : ZXBaseViewController
+
+@end
