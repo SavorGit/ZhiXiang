576ce60747e17bc9eca24681d843e99affe277e6
郭春城 <gccios@sina.com>
2017-09-20 18:31:02 +0800

    稍微的修改

--- ZhiXiang/ZhiXiang/Views/Home/HomeDetailView/SpecialHeaderView.m
+++ ZhiXiang/ZhiXiang/Views/Home/HomeDetailView/SpecialHeaderView.m
@@ -17,11 +17,11 @@
 
 @property (nonatomic, strong) UIImageView * bgImageView;
 
-@property (nonatomic, strong) UILabel *subTitleLabel;
+@property (nonatomic, assign) CGFloat maxImageHeight;
+@property (nonatomic, assign) CGFloat minImageHeight;
 
-@property (nonatomic, assign) CGFloat descHeight;
-@property (nonatomic, assign) CGFloat imageHeight;
-@property (nonatomic, assign) CGFloat tHeight;
+@property (nonatomic, assign) CGFloat maxTitleHeight;
+@property (nonatomic, assign) CGFloat minTitleHeight;
 
 @end
 
@@ -36,9 +36,10 @@
 
 - (void)creatSubViews{
     
-    _imageHeight =  kMainBoundsHeight - kStatusBarHeight - kNaviBarHeight - 30 - 70;
+    self.maxImageHeight =  kMainBoundsWidth / 750.f * 488.f;
+    self.minImageHeight = (kMainBoundsWidth - 60) / 750.f * 488.f;
     _bgImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
-    _bgImageView.frame = CGRectMake(0, 0, kMainBoundsWidth, _imageHeight/2);
+    _bgImageView.frame = CGRectMake(0, 0, kMainBoundsWidth, self.minImageHeight);
     _bgImageView.contentMode = UIViewContentModeScaleAspectFill;
     _bgImageView.layer.masksToBounds = YES;
     _bgImageView.backgroundColor = [UIColor clearColor];
@@ -53,7 +54,7 @@
 //    }];
     
     _titleLabel = [[UILabel alloc] init];
-    _titleLabel.frame = CGRectMake(15, _imageHeight/2 + 20, kMainBoundsWidth - 30, 31);
+    _titleLabel.frame = CGRectMake(15, self.minImageHeight + 20, kMainBoundsWidth - 30, 31);
     _titleLabel.backgroundColor = [UIColor clearColor];
     _titleLabel.font = kPingFangMedium(22);
     _titleLabel.textColor = UIColorFromRGB(0x434343);
@@ -68,13 +69,6 @@
 //        make.left.mas_equalTo(15);
 //    }];
     
-    _subTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, _imageHeight/2 + 20 + 31, kMainBoundsWidth - 30, 20)];
-    _subTitleLabel.text = @"";
-    _subTitleLabel.font = kPingFangLight(15);
-    _subTitleLabel.textColor = UIColorFromRGB(0x575757);
-    _subTitleLabel.backgroundColor = [UIColor clearColor];
-    _subTitleLabel.numberOfLines = 0;
-    [self addSubview:_subTitleLabel];
 //    [_subTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
 //        make.size.mas_equalTo(CGSizeMake(kMainBoundsWidth - 30, 20));
 //        make.top.mas_equalTo(_titleLabel.mas_bottom).offset(20);
@@ -96,10 +90,10 @@
 
 - (void)configModelData:(HomeViewModel *)model{
     
-    CGFloat titleHeight = [self getHeightByWidth:kMainBoundsWidth - 30 title:model.title font:kPingFangMedium(22)];
-    _tHeight = [self getHeightByWidth:kMainBoundsWidth - 30 - 60 title:model.title font:kPingFangMedium(22)];
+    self.maxTitleHeight = [self getHeightByWidth:kMainBoundsWidth - 30 title:model.title font:kPingFangMedium(22)];
+    self.minTitleHeight = [self getHeightByWidth:kMainBoundsWidth - 30 - 60 title:model.title font:kPingFangMedium(22)];
     CGFloat maxHeight = 31;
-    if (titleHeight > 31) {
+    if (self.minTitleHeight > 31) {
         maxHeight = 62;
 //        _titleLabel.frame = CGRectMake(15, _imageHeight/2 + 20, kMainBoundsWidth - 30, 62);
 //        [self.titleLabel mas_updateConstraints:^(MASConstraintMaker *make) {
@@ -112,33 +106,8 @@
 //            make.height.mas_equalTo(31);
 //        }];
     }
-    _titleLabel.frame = CGRectMake(15, _imageHeight/2 + 20, kMainBoundsWidth - 30, maxHeight);
+    _titleLabel.frame = CGRectMake(15, self.minImageHeight + 20, kMainBoundsWidth - 30, maxHeight);
     self.titleLabel.text = model.title;
-    
-    self.subTitleLabel.text = model.desc;
-//    [self configDescLabel];
-    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:self.subTitleLabel.text];
-    NSUInteger length = [self.subTitleLabel.text length];
-    [attrString addAttribute:NSFontAttributeName value:kPingFangLight(15) range:NSMakeRange(0, length)];//设置所有的字体
-    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    style.lineSpacing = 5;//行间距
-    style.headIndent = 0;//头部缩进，相当于左padding
-    style.tailIndent = 0;//相当于右padding
-    style.lineHeightMultiple = 1;//行间距是多少倍
-    style.alignment = NSTextAlignmentLeft;//对齐方式
-    style.firstLineHeadIndent = 0;//首行头缩进
-    style.paragraphSpacing = 30;//段落后面的间距
-    style.paragraphSpacingBefore = 0;//段落之前的间距
-    style.lineBreakMode = NSLineBreakByWordWrapping;// 分割模式
-    [attrString addAttribute:NSParagraphStyleAttributeName value:style range:NSMakeRange(0, length)];
-    [attrString addAttribute:NSKernAttributeName value:@2 range:NSMakeRange(0, length)];//字符间距 2pt
-    self.subTitleLabel.attributedText = attrString;
-    
-    // 计算富文本的高度
-    CGFloat descHeight = [self.subTitleLabel sizeThatFits:self.subTitleLabel.bounds.size].height;
-    self.descHeight = descHeight;
-    self.subTitleLabel.frame = CGRectMake(15, _imageHeight/2 + 20 + maxHeight + 20, kMainBoundsWidth - 30, descHeight);
-    
     
     if ([self.imageURL isEqualToString:model.imageURL]) {
         return;
@@ -146,44 +115,21 @@
     self.imageURL = model.img_url;
     [self.bgImageView sd_setImageWithURL:[NSURL URLWithString:model.img_url] placeholderImage:[UIImage imageNamed:@"zanwu"]];
     
+    self.frame = CGRectMake(0, 0, kMainBoundsWidth - 60, _titleLabel.frame.origin.y + _titleLabel.frame.size.height + 25);
 }
 
-- (void)configDescLabel{
+- (void)startScrShow
+{
+    _titleLabel.frame = CGRectMake(15, self.maxImageHeight + 20, kMainBoundsWidth - 30, self.maxTitleHeight);
+    self.frame = CGRectMake(0, 0, kMainBoundsWidth, _titleLabel.frame.origin.y + _titleLabel.frame.size.height + 25);
+    self.bgImageView.frame = CGRectMake(0, 0, kMainBoundsWidth, self.maxImageHeight);
     
-    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:self.subTitleLabel.text];
-    NSUInteger length = [self.subTitleLabel.text length];
-    [attrString addAttribute:NSFontAttributeName value:kPingFangLight(15) range:NSMakeRange(0, length)];//设置所有的字体
-    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
-    style.lineSpacing = 5;//行间距
-    style.headIndent = 0;//头部缩进，相当于左padding
-    style.tailIndent = 0;//相当于右padding
-    style.lineHeightMultiple = 1;//行间距是多少倍
-    style.alignment = NSTextAlignmentLeft;//对齐方式
-    style.firstLineHeadIndent = 0;//首行头缩进
-    style.paragraphSpacing = 30;//段落后面的间距
-    style.paragraphSpacingBefore = 0;//段落之前的间距
-    style.lineBreakMode = NSLineBreakByWordWrapping;// 分割模式
-    [attrString addAttribute:NSParagraphStyleAttributeName value:style range:NSMakeRange(0, length)];
-    [attrString addAttribute:NSKernAttributeName value:@2 range:NSMakeRange(0, length)];//字符间距 2pt
-    self.subTitleLabel.attributedText = attrString;
-    
-    // 计算富文本的高度
-    CGFloat descHeight = [self.subTitleLabel sizeThatFits:self.subTitleLabel.bounds.size].height;
-    self.descHeight = descHeight;
-    self.subTitleLabel.frame = CGRectMake(15, _imageHeight/2 + 20 + 31, kMainBoundsWidth - 30, 20);
-    
-//    [self.subTitleLabel mas_updateConstraints:^(MASConstraintMaker *make) {
-//        make.size.mas_equalTo(CGSizeMake(kMainBoundsWidth - 30, descHeight));
-//    }];
 }
+
 - (void)endScrShow{
-    
-    
-    _bgImageView.frame = CGRectMake(0, 0, kMainBoundsWidth - 60, _imageHeight/2);
-    
-    _titleLabel.frame = CGRectMake(15, _imageHeight/2 + 20, kMainBoundsWidth - 30 - 60, _tHeight);
-    
-    _subTitleLabel.frame = CGRectMake(15, _imageHeight/2 + 20 + _tHeight + 20, kMainBoundsWidth - 30 - 60, _descHeight);
+    _titleLabel.frame = CGRectMake(15, self.minImageHeight + 20, kMainBoundsWidth - 30 - 60, self.minTitleHeight);
+    self.frame = CGRectMake(0, 0, kMainBoundsWidth - 60, _titleLabel.frame.origin.y + _titleLabel.frame.size.height + 25);
+    self.bgImageView.frame = CGRectMake(0, 0, kMainBoundsWidth - 60, self.minImageHeight);
 }
 
 @end
