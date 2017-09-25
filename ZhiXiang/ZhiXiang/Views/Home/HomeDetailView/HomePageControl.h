5b7d2fd4e2663058751b811c6f6295e1c2046041
郭春城 <gccios@sina.com>
2017-09-25 15:53:44 +0800

    增加页码类

---
+++ ZhiXiang/ZhiXiang/Views/Home/HomeDetailView/HomePageControl.h
@@ -0,0 +1,13 @@
+//
+//  HomePageControl.h
+//  ZhiXiang
+//
+//  Created by 郭春城 on 2017/9/25.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePageControl : UIView
+
+@end
---
+++ ZhiXiang/ZhiXiang/Views/Home/HomeDetailView/HomePageControl.m
@@ -0,0 +1,21 @@
+//
+//  HomePageControl.m
+//  ZhiXiang
+//
+//  Created by 郭春城 on 2017/9/25.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import "HomePageControl.h"
+
+@implementation HomePageControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
